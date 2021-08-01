64409fa8c2db0e9bad65780060d00e6707883146
Michael Seales <heteromeles@users.noreply.github.com>
2021-08-01 14:08:43 -0700

    Added changes up to Chapter 5 Conditional Content problem

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,41 +1,26 @@
 import { useState } from "react";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
-import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+
+import "./Expenses.css";
 
 const Expenses = (props) => {
-  const [year, setYearTo] = useState("2020");
+  const [filterYear, setFilterYearTo] = useState("2020");
 
-  const yearFilterChangeHandler = (aSelectedYear) => {
-    console.log("yearFilterChangeHandler says " + aSelectedYear);
-    setYearTo(aSelectedYear);
+  const yearFilterChangeHandler = (aFilterYear) => {
+    setFilterYearTo(aFilterYear);
   };
+
+  const theFilteredItems = props.items.filter((item) => {
+    return item.date.getFullYear() === parseInt(filterYear);
+  });
 
   return (
     <div>
       <Card className="expenses">
-        <ExpensesFilter selectedYear={year} onYearFilterChange={yearFilterChangeHandler} />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        ></ExpenseItem>
+        <ExpensesFilter filterYear={filterYear} onYearFilterChange={yearFilterChangeHandler} />
+        <ExpensesList items={theFilteredItems} />
       </Card>
     </div>
   );
